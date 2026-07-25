dc677dee817de42cfa379731c6e04da4ad50b30d
previselogic-admin <previselogic@outlook.com>
2026-07-25 17:06:59 -0400

    Add script to connect to Microsoft Graph and retrieve group information

---
+++ Groups.ps1
@@ -0,0 +1,10 @@
+# Connect to Microsoft Graph
+Connect-MgGraph -Scopes "Group.Read.All"
+
+# Get all groups
+$groups = Get-MgGroup -All
+
+# Output the groups
+$groups |
+    Select-Object Id, DisplayName, Description, Mail, MailEnabled, SecurityEnabled |
+    Format-Table -AutoSize
